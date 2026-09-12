ced84513588a7f56295cf9e8a1676d3242b0571d
thanhtuan090404 <ntuan090404@gmail.com>
2026-09-12 22:00:43 +0700

    them rao can

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    [SerializeField] GameObject spearPrefabs; 
+    [SerializeField] float[] lanes =  { -2.5f, 0f, 2.5f }; // mảng vị trí spawn vật thể 
+    [SerializeField] private float yOffset = 0.91f; // khoảng cách spawn vật thể theo trục y
+
+
+    void Start()
+    {
+        SpawnSpear();
+    }
+
+    private void SpawnSpear()
+    {
+        List<int> availableLanes = new List<int> { 0, 1, 2 }; // danh sách các lane có sẵn
+        int spearToSpawn = UnityEngine.Random.Range(1, lanes.Length); // số lượng vật thể cần spawn (1 đến 3)
+
+        for (int i =0; i < spearToSpawn; i++)
+        {
+            int randomLaneIndex = UnityEngine.Random.Range(0, availableLanes.Count); // chọn ngẫu nhiên một lane từ danh sách có sẵn
+            int selectedLane = availableLanes[randomLaneIndex]; // lấy lane được chọn
+            availableLanes.RemoveAt(randomLaneIndex); // loại bỏ lane đã chọn khỏi danh sách có sẵn để tránh spawn trùng lặp
+            Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z); // tạo vị trí spawn dựa trên lane được chọn
+            Instantiate(spearPrefabs, spawnPosition, Quaternion.identity, this.transform); // spawn vật thể tại vị trí spawn
+
+
+        }
+    }
+}
--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject obstaclesPrefab; // object cho vật thể 
+    [SerializeField] private GameObject[] obstaclesPrefabs; // object cho vật thể 
     private float obstaclesSpawnTime = 2f; // tốc độ spawn vật thể
-    [SerializeField] private Transform swapPoint; 
+    [SerializeField] private float spawnWidth = 4f; // chiều rộng spawn vật thể
+    [SerializeField] private Transform obstaclesParent; 
+
+
+
 
     void Start()
     {
@@ -17,9 +21,11 @@ public class ObstacleSpawner : MonoBehaviour
         
         while (true)
         {
+            GameObject obstaclesPrefab = obstaclesPrefabs[Random.Range(0, obstaclesPrefabs.Length)]; // chọn ngẫu nhiên vật thể từ mảng obstaclesPrefabs
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnWidth, spawnWidth), transform.position.y, transform.position.z); // tạo vị trí spawn ngẫu nhiên trong phạm vi spawnWidth
             yield return new WaitForSeconds(obstaclesSpawnTime);
 
-            Instantiate(obstaclesPrefab, swapPoint.position, Random.rotation); // tạo vật thể tại vị trí spawn
+            Instantiate(obstaclesPrefab, spawnPosition, Random.rotation); // tạo vật thể tại vị trí spawn
         }
     }
     
